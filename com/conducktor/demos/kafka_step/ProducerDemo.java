8e0199ecf19969bd2c7848956192944c0c8241f4
Prabhat-git-99 <prabatwork3939@gmail.com>
2022-04-08 11:46:01 +0530

    Log4j added

--- com/conducktor/demos/kafka_step/ProducerDemo.java
+++ com/conducktor/demos/kafka_step/ProducerDemo.java
@@ -1,10 +1,17 @@
 package com.conducktor.demos.kafka_step;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ProducerDemo {
+	
+	private static final Logger log = LoggerFactory.getLogger( ProducerDemo.class.getSimpleName( ) );
+	
 
 	public static void main(String[] args) { 
 		// TODO Auto-generated method stub
-		System.out.println( "Hello world!!!" );
+		
+		log.info( "Hello world!!!" );
 		
 		//****************************
 		// Simple Program 
