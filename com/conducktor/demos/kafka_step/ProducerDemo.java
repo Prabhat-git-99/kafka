4f9bcad7d11ea174e526a87bcce6c8f44922823c
Prabhat-git-99 <prabatwork3939@gmail.com>
2022-04-08 11:39:17 +0530

    Template defined

--- com/conducktor/demos/kafka_step/ProducerDemo.java
+++ com/conducktor/demos/kafka_step/ProducerDemo.java
@@ -2,9 +2,26 @@ package com.conducktor.demos.kafka_step;
 
 public class ProducerDemo {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		// TODO Auto-generated method stub
 		System.out.println( "Hello world!!!" );
+		
+		//****************************
+		// Simple Program 
+		// to send data to kafka
+		//****************************
+		
+		// Create Producer Properties
+		
+		
+		// Create the Producer
+		
+		
+		// Send data
+		
+		
+		// Flush & Close the Producer
+		
 	}
 
 }
