81c3a06cae60fa7e7dfc9b30f4c820db18f19e69
Prabhat-git-99 <prabatwork3939@gmail.com>
2022-04-08 11:30:15 +0530

    Initialized Project

---
+++ com/conducktor/demos/kafka_step/App.java
@@ -0,0 +1,13 @@
+package com.conducktor.demos.kafka_step;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
