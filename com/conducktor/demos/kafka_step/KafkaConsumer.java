2132fe9ae250795f0a279dc7f796f20c4aeed240
Prabhat-git-99 <prabatwork3939@gmail.com>
2022-04-13 11:46:15 +0530

    Kafk consumer

--- com/conducktor/demos/kafka_step/KafkaConsumer.java
+++ com/conducktor/demos/kafka_step/KafkaConsumer.java
@@ -1,10 +1,18 @@
 package com.conducktor.demos.kafka_step;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class KafkaConsumer {
 
+	private static final Logger log = LoggerFactory.getLogger( ProducerDemo.class.getSimpleName( ) );
+
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		log.info( "I am kafka consumer" );
+		
 	}
 
 }
